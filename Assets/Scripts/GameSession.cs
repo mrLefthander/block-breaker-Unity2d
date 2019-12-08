2544e65bd0ae5832f7d83653948bcc40b5b01ae8
Lefthander <lefthander89@gmail.com>
2019-12-08 07:33:23 +0200

    full project commit

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour {   
+    //config params
+    [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoplayEnabled;
+
+    //state variables
+    [SerializeField] int currentScore = 0;
+
+    private void Awake() {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+        if(gameStatusCount > 1) {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        scoreText.text = currentScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore() {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame() {
+        gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+    public bool IsAutoPlayEnabled() {
+        return isActiveAndEnabled;
+    }
+
+}
